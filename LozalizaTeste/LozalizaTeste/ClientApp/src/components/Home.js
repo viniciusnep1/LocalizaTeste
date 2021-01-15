d4f0c5fa51beedb0c5144464d8a331fa664a8265
Vinicius <vinicius.barbosa@rehagro.com.br>
2021-01-15 11:23:02 -0300

    finish the test

--- LozalizaTeste/LozalizaTeste/ClientApp/src/components/Home.js
+++ LozalizaTeste/LozalizaTeste/ClientApp/src/components/Home.js
@@ -1,40 +1,67 @@
 import React, { Component } from 'react';
-import { Container, Button } from 'reactstrap';
+import { Container, Row, Col, ListGroupItem, ListGroup, Button, Form, FormGroup, Input, Label } from 'reactstrap';
 
 export class Home extends Component {
     static displayName = Home.name;
 
     constructor(props) {
         super(props);
-        this.state = { forecasts: [], loading: true };
+        this.state = {
+            data: {
+                primes: null,
+                divisors: null
+            }, divisor: ""
+        };
     }
 
 
-    async populateWeatherData() {
+    async fetchData() {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: 45
+            body: this.state.divisor
         };
 
         const response = await fetch('divisors', requestOptions);
         const data = await response.json();
-        console.log(data);
-        this.setState({ forecasts: data, loading: false });
+        console.log(data.result);
+        this.setState({ data: data.result });
     }
 
 
     render() {
 
         return (
-            <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                <Button type="button" onClick={this.populateWeatherData.bind(this)} > Botão </ Button>
+            <Container>
+                <h1 > Calcular Divisores </h1>
 
-            </div>
+                <Form>
+                    <FormGroup>
+                        <Label for="divisor">Digite um divisor:*</Label>
+                        <Input type="number" min="0" name="divisor" id="divisor" placeholder="Digite aqui" onChange={e => this.setState({ divisor: e.target.value, data: { primes: null, divisors: null } })} style={{ width: 250 }} />
+                    </FormGroup>
+                    <Button type="button" color="primary" active onClick={this.fetchData.bind(this)} style={{ marginBottom: 10 }}> Calcular </ Button>
+
+                </Form>
+                {
+                    this.state.data.divisors &&
+                    <Col md="12">
+                        <hr />
+                        <h3 > Resultado: </h3>
+                        <div>
+                            <span>Numero de Entrada:</span> {this.state.divisor}
+                        </div>
+                        <div>
+                            <span>Numero de Divisores:</span> {this.state.data.divisors}
+                        </div>
+                        <div>
+                            <span>Divisores Primos:</span> {this.state.data.primes}
+                        </div>
+                    </Col>
+                }
+            </Container>
         );
     }
 
-    
+
 }
--- LozalizaTeste/LozalizaTeste/ClientApp/src/components/NavMenu.js
+++ LozalizaTeste/LozalizaTeste/ClientApp/src/components/NavMenu.js
@@ -26,7 +26,7 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">LozalizaTeste</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">Lozaliza Teste</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           </Container>
         </Navbar>
